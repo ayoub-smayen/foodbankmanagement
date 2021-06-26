0e0b4ae71acc87e6c6e6d2f9b95c32f346f39456
ayoub-smayen <ayoubjobs.2019@gmail.com>
2021-06-27 01:09:39 +0200

    heroku1

--- FoodBank/settings.py
+++ FoodBank/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Your email'
-EMAIL_HOST_PASSWORD = 'your email password'
+EMAIL_HOST_USER = 'ayoubjobs.2019@gmail.com'
+EMAIL_HOST_PASSWORD = 'sahar+ayoub1'
